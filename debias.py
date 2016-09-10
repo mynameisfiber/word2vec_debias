c37b48ebe73c9ef29871bbf9301d8dc8342b7071
Micha Gorelick <mynameisfiber@gmail.com>
2016-09-10 20:51:43 +0000

    added scipy bias correction solver

--- debias.py
+++ debias.py
@@ -5,12 +5,13 @@ Implementation of http://arxiv.org/abs/1607.06520
 import random
 import numpy as np
 import cvxpy as cvx
+from scipy import optimize
 
 from functools import wraps
 import itertools as it
 import time
 
-from word2vec import load_word2vec_model
+from word2vec import load_word2vec_model, trim_model
 
 
 def timer(fxn):
@@ -61,12 +62,17 @@ def soft_bias_correction(model, gender_subspace, neutral_words, tuning=0.2):
     E = np.diag(E)
     I = np.eye(E.shape[0])
     UE = U @ E
+    B = gender_subspace
 
+    return _solve_soft_bias_correction_scipy(UE, I, N, B, tuning)
+
+
+def _solve_soft_bias_correction_cvxpy(UE, I, N, B, tuning):
     X = cvx.Semidef(N.shape[0], name='T')
-    objective = sum((
-        cvx.Minimize(cvx.sum_squares(UE.T * (X - I) * UE)),
-        cvx.Minimize(tuning * cvx.sum_squares(N.T * X * B.T))
-    ))
+    objective = (
+        cvx.Minimize(cvx.sum_squares(UE.T * (X - I) * UE) +
+                     cvx.quad_over_lin(N.T * X * B.T), tuning)
+    )
     constraints = [X >> 0]
     prob = cvx.Problem(objective, constraints)
 
@@ -78,17 +84,43 @@ def soft_bias_correction(model, gender_subspace, neutral_words, tuning=0.2):
             pass
 
     prob.solve(solver=cvx.SCS, verbose=True)
-
     return X.value, prob.value
+
+
+def _solve_soft_bias_correction_scipy(UE, I, N, B, tuning):
+    def objective(x, X, UE, I, N, B, tuning):
+        X[np.triu_indices(N.shape[0], -1)] = 0
+        X[np.triu_indices(N.shape[0])] = x
+        X = X + X.T - np.diag(X.diagonal())
+        return (np.linalg.norm(UE.T @ (X - I) @ UE)**2 +
+                tuning * np.linalg.norm(N.T @ X @ B.T)**2)
+
+    n_elements = N.shape[0] * (N.shape[0] + 1)/2
+    x = np.random.random(n_elements)
+    X = np.zeros((N.shape[0],)*2)
+
+    constraints = ({'type': 'ineq', 'fun': lambda x: x})
+    result = optimize.minimize(objective, x, args=(X, UE, I, N, B, tuning),
+                               constraints=constraints,
+                               options={'disp': True, 'maxiter': 1000000},
+                               method='COBYLA')
+
+    x = result.x
+    X[np.triu_indices(N.shape[0], -1)] = 0
+    X[np.triu_indices(N.shape[0])] = x
+    X = X + X.T - np.diag(X.diagonal())
+    return X, objective(result.x)
 
 
 if __name__ == "__main__":
     gendered_words = {w.strip().split(',')[0]
                       for w in it.chain(open("gendered_words_classifier.txt"),
                                         open("gendered_words.txt"))}
-    model = load_word2vec_model(truncate_vector=100)
+    model = load_word2vec_model(truncate_vector=50)
     gendered_words = list(filter(model.vocab.__contains__, gendered_words))
     neutral_words = set(model.vocab) - set(gendered_words)
+    model = trim_model(model, neutral_words[1000:])
+    neutral_words = neutral_words[:1000]
 
-    B = gender_subspace(model, [gendered_words])
+    B = gender_subspace(model, [gendered_words], k=4)
     X, result = soft_bias_correction(model, B, neutral_words)
