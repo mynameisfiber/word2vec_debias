c8ff3d1620bcb558d724ace7920326de39e271a6
Micha Gorelick <mynameisfiber@gmail.com>
2016-09-10 04:30:42 +0000

    iterating

--- debias.py
+++ debias.py
@@ -6,20 +6,25 @@ import random
 from gensim.models import Word2Vec
 import numpy as np
 import cvxpy as cvx
+from tqdm import tqdm
+from joblib import Memory
 
 import re
 from functools import wraps
 import time
 import heapq
 
+memory = Memory(cachedir='./cache/', verbose=1)
+
 
 def timer(fxn):
+    @wraps(fxn)
     def _(*args, **kwargs):
-        print("Running {}".format(fxn.__name__), end='', flush=True)
+        print("Starting {}".format(fxn.__name__))
         start = time.time()
         result = fxn(*args, **kwargs)
         runtime = time.time() - start
-        print(": took {:0.3f}s".format(runtime))
+        print("{}: took {:0.3f}s".format(fxn.__name__, runtime))
         return result
     return _
 
@@ -37,18 +42,16 @@ def gender_subspace(model, k=10):
 @timer
 def soft_bias_correction(model, gender_subspace, neutral_words, tuning=0.2):
     neutral_indexes = [model.vocab[w].index for w in neutral_words]
-
-    W = model.syn0.T
-    U, E, Vt = np.linalg.svd(W, full_matrices=False)
+    N = model.syn0.T[:, neutral_indexes]
+    # slice the svd to ignore the unitary matricies
+    U, E = np.linalg.svd(model.syn0.T, full_matrices=False)[:-1]
     E = np.diag(E)
+    I = np.eye(E.shape[0])
     UE = U @ E
 
-    N = W[:, neutral_indexes]
-    I = np.eye(W.shape[0])
-
-    X = cvx.Semidef(W.shape[0], name='X')
+    X = cvx.Semidef(N.shape[0], name='T')
     objective = sum((
-        cvx.Minimize(cvx.sum_squares(UE.T * (X-I) * UE)),
+        cvx.Minimize(cvx.sum_squares(UE.T * (X - I) * UE)),
         cvx.Minimize(tuning * cvx.sum_squares(N.T * X * B.T))
     ))
     constraints = [X >> 0]
@@ -61,37 +64,47 @@ def soft_bias_correction(model, gender_subspace, neutral_words, tuning=0.2):
         except AttributeError:
             pass
 
-    prob.solve(solver=cvx.SCS, verbose=True)
+    prob.solve(solver=cvx.CVXOPT, verbose=True)
 
     return X.value, prob.value
 
 
 @timer
-def vocab_subsample(model, top_k=25, max_length=20):
+def vocab_subsample(model, top_k=5000, max_length=20):
     nlargest = []
     acceptable = re.compile("^[a-z ]{," + str(max_length) + "}$")
-    for word, value in model.vocab.items():
+    for word, value in tqdm(model.vocab.items()):
         if acceptable.match(word) is not None:
             item = (value.count, (word, value))
             if len(nlargest) >= top_k:
                 heapq.heappushpop(nlargest, item)
             else:
                 heapq.heappush(nlargest, item)
-    indexes = [value.index for _, (word, value) in nlargest]
+    indexes = []
+    model.vocab = {}
+    for i, (_, (word, value)) in enumerate(nlargest):
+        indexes.append(value.index)
+        value.index = i
+        model.vocab[word] = value
     model.syn0 = model.syn0[indexes]
-    model.vocab = {word:value for _, (word, value) in nlargest}
+    return model
+
+
+@timer
+@memory.cache
+def load_word2vec_model():
+    model = timer(Word2Vec.load_word2vec_format)(
+        './data/word2vec_googlenews_negative300.bin',
+        binary=True, limit=None
+    )
+    model.syn0 = model.syn0[:, :100]
+    model.syn0 /= np.linalg.norm(model.syn0)  # ensure normalied
+    model = vocab_subsample(model, top_k=50000)
     return model
 
 
 if __name__ == "__main__":
-    model = Word2Vec.load_word2vec_format(
-        './data/word2vec_googlenews_negative300.bin',
-        binary=True, limit=1000
-    )
-    model.syn0 = model.syn0[:, :100]
-    model.syn0 /= np.linalg.norm(model.syn0)  # ensure normalied
-    model = vocab_subsample(model)
-
+    model = load_word2vec_model()
     B = gender_subspace(model)
     neutral_words = random.sample(model.vocab.keys(), 5)
     X, result = soft_bias_correction(model, B, neutral_words)
