e25a8d0e4eb4d0da384b086362a7e714e44fe9cb
Micha Gorelick <mynameisfiber@gmail.com>
2016-09-10 03:46:13 +0000

    trying to get this to run on a massive aws machine

--- debias.py
+++ debias.py
@@ -6,7 +6,6 @@ import random
 from gensim.models import Word2Vec
 import numpy as np
 import cvxpy as cvx
-import cvxopt
 from functools import wraps
 import time
 
@@ -35,21 +34,31 @@ def gender_subspace(model, k=10):
 @timer
 def soft_bias_correction(model, gender_subspace, neutral_words, tuning=0.2):
     neutral_indexes = [model.vocab[w].index for w in neutral_words]
+
     W = model.syn0.T
-    U, E, Vt = np.linalg.svd(W)
+    U, E, Vt = np.linalg.svd(W, full_matrices=False)
     E = np.diag(E)
+    UE = U @ E
+
     N = W[:, neutral_indexes]
     I = np.eye(W.shape[0])
-    UE = U @ E
 
     X = cvx.Semidef(W.shape[0], name='X')
     objective = sum((
         cvx.Minimize(cvx.sum_squares(UE.T * (X-I) * UE)),
         cvx.Minimize(tuning * cvx.sum_squares(N.T * X * B.T))
     ))
-    constraints = [0 <= X]
+    constraints = [X >> 0]
     prob = cvx.Problem(objective, constraints)
-    prob.solve(solver=cvx.SCS, verbose=True, use_indirect=False)
+
+    print("Large constants:")
+    for c in prob.constants():
+        try:
+            print("\tConstant of size:", c.value.shape, type(c.value))
+        except AttributeError:
+            pass
+
+    prob.solve(solver=cvx.SCS, verbose=True)
 
     return X.value, prob.value
 
@@ -57,8 +66,9 @@ def soft_bias_correction(model, gender_subspace, neutral_words, tuning=0.2):
 if __name__ == "__main__":
     model = Word2Vec.load_word2vec_format(
         './data/word2vec_googlenews_negative300.bin',
-        binary=True, limit=500
+        binary=True, limit=1000
     )
+    model.syn0 = model.syn0[:, :100]
     B = gender_subspace(model)
     neutral_words = random.sample(model.vocab.keys(), 5)
-    soft_bias_correction(model, B, neutral_words)
+    X, result = soft_bias_correction(model, B, neutral_words)
